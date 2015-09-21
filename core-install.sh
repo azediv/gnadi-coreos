6ccea1e2de28a6e307d8a0a3e9fd0856176803df
aze <jonathan.grech@gandi.net>
2015-09-21 08:47:28 -0400

    core-install.sh

---
+++ core-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+VM=coreos
+
+# Création VM Debian (au moins 512Mo, 256Mo pas suffisant pour install des packages)
+
+gandi vm create --datacenter US --memory 512 --cores 1 --ip-version 4   --hostname $VM --image "Debian 8 64 bits (HVM$
+
+sleep 60
+
+# Création disque data (cible de l'install de CoreOS) et attachement à la VM :
+
+gandi disk create --name core_sys --size 10G --datacenter US -vm $VM
+
+sleep 30
+
+# Connexion SSH à Debian (pour effectuer l'installation et récupérer la configuration de l'interface réseau eth0) :
+
+# Démontage du disque data avant l'installation :
+
+gandi vm ssh $VM "umount /dev/sdc;\
+
+apt-get update && apt-get install -y wget &&\
+
+wget https://raw.github.com/coreos/init/master/bin/coreos-install &&\
+
+wget https://raw.githubusercontent.com/azediv/gnadi-coreos/master/if-core-config.sh &&\
+
+chmod +x if-core-config.sh &&\
+
+chmod +x coreos-install"
+
+IP=`gandi vm info $VM | grep ip4 | sed 's/ip4 *: //g'`
+
+printf "ssh_authorized_keys:\n - " > base_cloud_config.yml
+
+cat ~/.ssh/id_rsa.pub >> base_cloud_config.yml
+
+scp base_cloud_config.yml root@$IP:/root/base-cloud-config.yml
+
+gandi vm ssh $VM "./if-core-config.sh &&\
+
+cat base-cloud-config.yml >> cloud-config.yml
+
