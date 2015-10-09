5723e0c20baabdc31526bf86ad6f2779ab477e6a
aze <jonathan.grech@gandi.net>
2015-10-09 14:55:55 -0400

    core-config done

--- cloud-config.sh
+++ cloud-config.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
+IP=`cat /gandi/config | grep -Po '(?<="pna_address": ")[^"]*' | head -1`
 
+ROUTE=`cat /gandi/config | grep -Po '(?<="pbn_gateway": ")[^"]*' | head -1`
+
+DNS=`cat /etc/resolv.conf | awk '{ print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1`
+
+PASS=`cat /gandi/config | grep -Po '(?<="password": ")[^"]*'`
+
+USER=`cat /gandi/config | grep -Po '(?<="user": ")[^"]*'`
+
+SSH=`cat /gandi/config | grep -Po '(?<="ssh_key": ")[^"]*'`
+
+printf "#cloud-config
+coreos:
+  units:
+    - name: systemd-networkd.service
+      command: stop
+    - name: 00-eth0.network
+      runtime: true
+      content: |
+        [Match]
+        Name=eth0
+
+        [Network]
+        DNS=$DNS
+        Address=$IP
+        Gateway=$ROUTE
+    - name: systemd-networkd.service
+      command: start
+
+users:
+  - name: $USER
+    passwd: $PASS
+    groups:
+      - sudo
+
+    ssh_authorized_keys:
+      - ssh-rsa $SSH" > cloud-config.yml
+
+exit
