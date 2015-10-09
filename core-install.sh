fdb13655b047a738a137a054e7dabf6295736b3d
aze <jonathan.grech@gandi.net>
2015-10-08 20:54:01 -0400

    modif core-install

--- core-install.sh
+++ core-install.sh
@@ -2,17 +2,19 @@
 
 VM=coreos
 
+DC=US
+
 # Création VM Debian (au moins 512Mo, 256Mo pas suffisant pour install des packages)
 
-gandi vm create --datacenter US --memory 512 --cores 1 --ip-version 4   --hostname $VM --image "Debian 8 64 bits (HVM$
+gandi vm create --datacenter $DC --memory 512 --cores 1 --ip-version 4  --hostname $VM --image 'Debian 8 64 bits (HVM)'
 
-sleep 60
+wait
 
 # Création disque data (cible de l'install de CoreOS) et attachement à la VM :
 
-gandi disk create --name core_sys --size 10G --datacenter US -vm $VM
+gandi disk create --name core_sys --size 10G --datacenter $DC --vm $VM
 
-sleep 30
+wait
 
 # Connexion SSH à Debian (pour effectuer l'installation et récupérer la configuration de l'interface réseau eth0) :
 
@@ -41,4 +43,3 @@ scp base_cloud_config.yml root@$IP:/root/base-cloud-config.yml
 gandi vm ssh $VM "./if-core-config.sh &&\
 
 cat base-cloud-config.yml >> cloud-config.yml
-
