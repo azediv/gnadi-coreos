7ae6ce5ebcea917018010429b76a247ef89dbb81
aze <jonathan.grech@gandi.net>
2015-10-09 04:49:39 -0400

    core add dc, if route head 1

--- core-install.sh
+++ core-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Définition du hostname de la vm
+
 VM=coreos
+
+# Choix du datacenter
 
 DC=US
 
@@ -16,9 +20,12 @@ gandi disk create --name core_sys --size 10G --datacenter $DC --vm $VM
 
 wait
 
-# Connexion SSH à Debian (pour effectuer l'installation et récupérer la configuration de l'interface réseau eth0) :
-
-# Démontage du disque data avant l'installation :
+# Connexion SSH à Debian 
+# Detach coreos disk before installation :
+# Get coreos-install script
+# Get interface configuration script for cloud-config
+#
+# TODO : get config from /gandi/config for cloud-config instead of a script
 
 gandi vm ssh $VM "umount /dev/sdc;\
 
@@ -32,14 +39,24 @@ chmod +x if-core-config.sh &&\
 
 chmod +x coreos-install"
 
+# Define vm IP address to scp base config file
+
 IP=`gandi vm info $VM | grep ip4 | sed 's/ip4 *: //g'`
+
+# Add ssh key to cloud-config
 
 printf "ssh_authorized_keys:\n - " > base_cloud_config.yml
 
 cat ~/.ssh/id_rsa.pub >> base_cloud_config.yml
 
+# Upload cloud-config
+
 scp base_cloud_config.yml root@$IP:/root/base-cloud-config.yml
+
+# SSH to vm 
 
 gandi vm ssh $VM "./if-core-config.sh &&\
 
-cat base-cloud-config.yml >> cloud-config.yml
+cat base-cloud-config.yml >> cloud-config.yml &&\
+
+"
